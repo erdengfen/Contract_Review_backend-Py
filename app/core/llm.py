a5db3171a8f13e2a3b94b795e10231c0cdb52b99
erdengfen <zhangbowei718@gmail.com>
2026-03-30 23:04:39 +0800

    refactor: 修改llm client初始化结构，仅在初始化时填入key和base

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,54 +1,46 @@
 """
 LLM初始化模块
 """
-
-from langchain_openai import ChatOpenAI
+from aiohttp.log import client_logger
+from openai import OpenAI
 from app.config.config import settings
 from openai import AsyncClient
-from httpx import Client
 from app.schemas.model_configs import ModelConfigResponse
 
 
 def init_llm():
-    """初始化LLM，支持通过配置切换provider与model"""
+    """
+    初始化LLM，model字段在调用时传入
+    :return:
+    """
     # provider = getattr(settings.openai_config, "provider", "deepseek")
-    model = settings.openai_config.model
+    # model = settings.openai_config.model
     api_key = settings.openai_config.api_key
     api_base = settings.openai_config.api_base
     if not api_key:
         raise ValueError("LLM API密钥未设置")
-    print(api_key, api_base, model)
+    if not api_base:
+        raise ValueError("BASE_URL未设置")
+    # print(api_key, api_base, model)
 
-    # http_client = Client(timeout=30.0)
-
-    return ChatOpenAI(
-        model_name=model,
-        openai_api_key=api_key,
-        openai_api_base=api_base,
-        temperature=0.7,
-        max_tokens=512,
-        timeout=15.0,
-        # http_client=http_client,
+    client = OpenAI(
+        api_key=api_key,
+        base_url=api_base,
     )
-#from langchain.chat_models import init_chat_model
-#
-# def init_llm(api_key: str, model: str, base_url: str = None, model_provider: str = "deepseek"):
-#     """
-#     初始化 LLM 客户端（支持不同模型与 API key）
-#     """
-#     if not api_key:
-#         raise ValueError("api_key 未设置")
-#
-#     # 初始化模型
-#     return init_chat_model(
-#         model=model,
-#         temperature=0,
-#         model_provider=model_provider,
-#         base_url=base_url,
-#         api_key=api_key
-#     )
 
+    return client
+
+    # return ChatOpenAI(
+    #     model_name=model,
+    #     openai_api_key=api_key,
+    #     openai_api_base=api_base,
+    #     temperature=0.7,
+    #     max_tokens=512,
+    #     timeout=15.0,
+    #     # http_client=http_client,
+    # )
 # ----------------------------------
+# 以下为保留方法
 def init_chat_model(
         api_key: str,
         api_base: str
